32d820ced24819e62e68fe90153bf7a3033f71d5
dikhod <103700464+dikhod@users.noreply.github.com>
2023-01-05 17:40:35 +0530

    Create Bulls and Cows.cpp

---
+++ https:/leetcode.com/problems/bulls-and-cows/Bulls and Cows.cpp
@@ -0,0 +1,26 @@
+  string getHint(string secret, string guess) {
+
+        int Bulls=0; 
+        int Cows=0;
+        unordered_map<char,int>s;
+        unordered_map<char,int>g;
+        for(int i=0;i<secret.length();i++)
+           {
+               if(secret[i]==guess[i])
+                   Bulls++;
+
+               else
+               {
+                    s[secret[i]]++;
+                    g[guess[i]]++;
+               }
+           }
+
+          for(auto p:s)
+          {
+              if(g.find(p.first)!=g.end())
+                Cows+=min(p.second,g[p.first]);
+          }   
+
+        return to_string(Bulls)+"A"+to_string(Cows)+"B";
+    }
